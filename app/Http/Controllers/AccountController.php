05e3e1e6b644969c9e948705438a76f223be8697
liepsnele <liepsnele.tech@gmail.com>
2023-01-30 00:25:45 +0200

    Update

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Account;
+use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\Http\Request;
 
 class AccountController extends Controller
